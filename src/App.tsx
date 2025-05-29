5ef44612c18b1f439435418b162e5bedd47445c9
zvolin <mac.zwolinski@gmail.com>
2025-05-29 15:55:47 +0200

    working grpc submit blob

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,19 @@
-import { useState, useEffect, useContext } from 'react'
+import { useState, useContext } from 'react'
 import { LuminaContext, LuminaContextProvider } from './Lumina.tsx'
-import { Network, NodeConfig, PeerTrackerInfoSnapshot, SyncingInfoSnapshot } from 'lumina-node'
+import { LeapClientContext, LeapClientContextProvider } from './LeapClient.tsx'
+import { AppVersion, Blob, Namespace, Network, NodeConfig, TxInfo } from 'lumina-node'
 import './App.css'
 
 function App() {
   return (
-    <LuminaContextProvider>
-      <main>
-        <Launcher />
-        <Stats />
-      </main>
-    </LuminaContextProvider>
+    <LeapClientContextProvider>
+      <LuminaContextProvider>
+        <main>
+          <Launcher />
+          <Stats />
+        </main>
+      </LuminaContextProvider>
+    </LeapClientContextProvider>
   )
 }
 
@@ -26,8 +29,6 @@ function Launcher() {
           onClick={async () => {
             let config = NodeConfig.default(Network.Mocha);
             config.bootnodes = [
-              "/dnsaddr/da-bootstrapper-1-mocha-4.celestia-mocha.com/p2p/12D3KooWCBAbQbJSpCpCGKzqz3rAN4ixYbc63K68zJg9aisuAajg",
-              "/dnsaddr/da-bootstrapper-2-mocha-4.celestia-mocha.com/p2p/12D3KooWCUHPLqQXZzpTx1x3TAsdn3vYmTNDhzg66yG8hqoxGGN8",
               "/dnsaddr/mocha-boot.pops.one/p2p/12D3KooWDzNyDSvTBdKQAmnsUdAyQCQWwM3ReXTmPaaf6LzfNwRs",
               "/dnsaddr/celestia-mocha.qubelabs.io/p2p/12D3KooWQVmHy7JpfxpKZfLjvn12GjvMgKrWdsHkFbV2kKqQFBCG",
               "/dnsaddr/celestia-mocha4-bootstrapper.binary.builders/p2p/12D3KooWK6AYaPSe2EP99NP5G2DKwWLfMi6zHMYdD65KRJwdJSVU",
@@ -47,41 +48,26 @@ function Launcher() {
 }
 
 function Stats() {
-  const node = useContext(LuminaContext);
+  const txClient = useContext(LeapClientContext);
+  const [_, setTxInfo] = useState<TxInfo | null>(null);
 
-  const [peerTrackerInfo, setPeerTrackerInfo] = useState<PeerTrackerInfoSnapshot | null>();
-  const [syncerInfo, setSyncerInfo] = useState<SyncingInfoSnapshot | null>();
-
-  useEffect(() => {
-    const interval = setInterval(() => {
-      const update = async () => {
-        if (node && await node.isRunning()) {
-          setPeerTrackerInfo(await node.peerTrackerInfo());
-          setSyncerInfo(await node.syncerInfo());
-        }
-      };
-      update();
-    }, 1000);
-    return () => clearInterval(interval);
-  }, [node]);
-
-  if (!peerTrackerInfo || !syncerInfo) {
-    return;
+  if (!txClient) {
+    return <div>Connect leap wallet to proceed</div>;
   }
 
   return (
-    <div>
-      <div>network head: {syncerInfo.subjective_head.toString()}</div>
-      <div>
-        stored headers:
-        {syncerInfo.stored_headers.map((range) => {
-          return ` ${range.start}..${range.end}`;
-        })}
-      </div>
-      <div>
-        peers: {peerTrackerInfo.num_connected_peers.toString()} (
-        {peerTrackerInfo.num_connected_trusted_peers.toString()} trusted)
-      </div>
-    </div>
+    <button
+      onClick={async () => {
+        const ns = Namespace.newV0(new Uint8Array([97, 98, 99]));
+        const data = new Uint8Array([100, 97, 116, 97]);
+        const blob = new Blob(ns, data, AppVersion.latest());
+
+        const txInfo = await txClient.submitBlobs([blob]);
+        console.error("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!", txInfo);
+        setTxInfo(txInfo);
+      }}
+    >
+      Submit blob
+    </button>
   );
 }
